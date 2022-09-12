cceb1fa9479b0e68f7acef2d035442b51a6e90a3
PaulManRobertson <bigwallpaul514@gmail.com>
2022-09-12 13:46:58 -0600

    loop practice assignments

---
+++ level1/exercises/loop-practice/loop.js
@@ -0,0 +1,49 @@
+
+//exercise 1)
+
+var officeItems = ["stapler", "monitor", "computer", "desk", "lamp", "computer", "lamp", "stapler", "computer",  "computer"]
+let count = 0;
+
+for( let i = 0; i < officeItems.length; i++ ){
+    if(officeItems[i] === "computer"){
+        count ++;
+    }
+}
+
+console.log(count);
+
+//exercise 2)
+
+var peopleWhoWantToSeeMadMaxFuryRoad = [
+  {
+    name: "Mike",
+    age: 12,
+    gender: "male"
+  },{
+    name: "Madeline",
+    age: 80,
+    gender: "female"
+  },{
+    name: "Cheryl",
+    age: 22,
+    gender: "female"
+  },{
+    name: "Sam",
+    age: 30,
+    gender: "male"
+  },{
+    name: "Suzy",
+    age: 4,
+    gender: "female"
+  }
+]
+for( let i = 0; i < peopleWhoWantToSeeMadMaxFuryRoad.length; i++ ){
+    if(peopleWhoWantToSeeMadMaxFuryRoad[i].age >= 18){
+        console.log(peopleWhoWantToSeeMadMaxFuryRoad[i].name + " is old enough to see Mad Max: Fury Road :)")
+    } else {
+        console.log(peopleWhoWantToSeeMadMaxFuryRoad[i].name + " is not old enough not see Mad Max: Fury Road :( ")
+    }
+}
+
+//exercise 3)
+
