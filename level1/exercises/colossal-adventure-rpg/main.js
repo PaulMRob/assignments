2f372770088297ab0f8f6b517738570a7da75655
PaulManRobertson <bigwallpaul514@gmail.com>
2022-09-28 08:43:14 -0600

    rpg game

--- level1/exercises/colossal-adventure-rpg/main.js
+++ level1/exercises/colossal-adventure-rpg/main.js
@@ -1,106 +1,151 @@
 const readline = require("readline-sync");
-    
-const greeting = readline.question("Greetings Aspriring Human. Today begins your proving.");
-const name = readline.question("Please give us your name.");
-let walking = readline.question("Let's take a walk [W]");
+let isAlive = true;
 
-let enemyHP = 10;
-let battle = false;
 
-function Player(){
-this.PlayerName = name
-this.hp = 20
-this.inventory = []
+const enemyArmy = ["GREG", "SHELBY", "STEVE", "URSULA", "KEVIN"];
+const booty = ["The Black Scroll of Futility", "The Black Scroll of Ecstasy", "The Black Scroll of Brutality", "The Black Scroll of Shattered Virtues", "The Black Scroll of Havoc"];
+const scrollLore = [
+    "The Black Scroll of Futility will confound the minds of your enemies",
+    "The Black Scroll of Ecstasy will capture the hearts of your captors so that you may control them.",
+    "The Black Scroll of Brutality will give you bear-like strength in battle",
+    "The Black Scroll of Shattered Virtues will cut off your enemies from the favor of their gods!",
+    "The Black Scroll of Havoc will plunge the world into unending chaos."
+];
+
+//random number generator!
+function getRandomNum(max) {
+    return Math.floor(Math.random() * Math.floor(max));
 }
 
-let enemies = [
-{
-name: 'greg',
-hp: 10,
-item: 'Glass'
-},
-]
+// Game Starts!
+const name = readline.question("Greetings aspiring Human. Today your prooving begins. You may now surrender your NAME.");
+console.clear();
 
-while (userHP > 0) {
-    walk();
+function startGame() {
+    console.log(`Greetings ${name}. My name is Morpheus and I am here to prepare you for the road ahead.`);
+    console.log(`You will pass throuh many perrils. There are many who would do you harm.`);
+    console.log(`But if you can collect all five of the black scrolls and bind Daemon Debby who will surely come for them...`);
+    console.log(`Then you will win the day.` );
+    console.log(`Your prize will be a magical kitten who never grows up and squeaks like a pika when you squeeze it hard enough.`)
+    console.log(`But who am i kidding? You won't survive ten minutes out there in the MAZE ;))))))))))))))`);
+};
+
+function Player() {
+    this.playerName = name;
+    this.hp = 50;
+    this.inventory = [];
 }
 
-// handles walking
-function walk() {
-    let evilChaos = (Math.floor(Math.random() * 3) + 1);
-    console.log("walk is running");
-    if (evilChaos === 2) {
-        console.log("You encounter a Wild Enemy.");
-        return enemyEncounter();
-    } else {
-        readline.question(`you walked ${Math.floor(Math.random() * 100)} meters.`);
+function Enemy(enemyName, enemyHP, booty) {
+    this.enemyName = enemyName;
+    this.enemyHP = enemyHP;
+    this.booty = booty;
+}
+
+let activePlayer = new Player();
+
+function attack(activeEnemy, isFighting) {
+
+    if (activeEnemy.enemyHP <= 0) {
+        console.log("Your enemy is Dead. You're a murderer :(");
+        activePlayer.hp += 5;
+        activePlayer.inventory.push(activeEnemy.booty);
+        console.log("... but at least you can have their stuff now :)");
+        isFighting = !isFighting
     }
+
+    if (activeEnemy.enemyHP > 0) {
+        console.log(`${activeEnemy.enemyName} now has ${activeEnemy.enemyHP} health :P`);
+        console.log("enemy hp:" + activeEnemy.enemyHP);
+    }
+
+    if (activePlayer.hp > 0) {
+        activePlayer.hp -= getRandomNum(20);
+        console.log(`You're a figther i guess. But you now have ${activePlayer.hp} health :/`);
+    }
+
+    if (activePlayer.hp <= 0) {
+        console.log(`You're dead ${activePlayer.playerName}. Sorry, there is no afterlife :(`);
+        isAlive = !isAlive;
+        process.exit();
+    }
+    return isFighting;
 }
 
 // handles encounter with wild enemy
 function enemyEncounter() {
-    const wildEnemyArmy = ["Greg", "Shelby", "Steve", "Ursula"];
-    let wildEnemy = wildEnemyArmy[Math.floor(Math.random() * wildEnemyArmy.length)];
-    console.log(`${wildEnemy} is about to attack!`);
-    let response = readline.question("Will you [r]un or [f]ight?");
-    if (response === "r") {
-        return run();
-    } else if (response === "f") {
-        return fight();
-    }
-    return wildEnemy;
-}
+    const enemyName = enemyArmy[getRandomNum(enemyArmy.length)];
+    const enemyBooty = booty[getRandomNum(booty.length)];
+    let activeEnemy = new Enemy(enemyName, 10, enemyBooty);
+    console.log(`${activeEnemy.enemyName} is about to attack :O`);
+    let isFighting = true;
+    // responseToAttack = ["fight", "run"]; //switch below to keyIn like you're using in walk()
 
-// handles running away
-function run() {
-    let chances = Math.floor(Math.random() * 2) + 1;
-    if (chances === 2) {
-        return enemyAttack();
-    } else {
-        return walk();
-    }
-}
-
-//handles fighting turns 
-function fight() {
-    while (!battle) {
-        userAttack();
-        if (enemyHP <= 0) {
-            console.log("Wild Enemy is Dead. You're a murderer :(");
-            prizeHandler();
+    while (isFighting) {
+        let fightOrRun = readline.keyIn(`will you fight or will you run? :} "r" = run, "f" = fight     `);
+        if (fightOrRun === "f") {
+            activeEnemy.enemyHP -= getRandomNum(10);
+            isFighting = attack(activeEnemy, isFighting);
+            console.log("enemy hp: " + activeEnemy.enemyHP);
+            console.log("player HP: " + activePlayer.hp);
         }
-        enemyAttack();
-        if (userHP <= 0) {
-            console.log("You're dead. Sorry, there is no afterlife :(");
-            // fight is terminated to end loop. 
+
+        else if (fightOrRun === "r") {
+            console.log("Running is not a cowardly act. But a futile one :P");
+            const chancesOfEscape = Math.random();
+            if (chancesOfEscape <= 0.5) {
+                console.log("Looks like you managed to escape... for now ;)");
+                return
+            }
+            else if (chancesOfEscape <= 1) {
+                console.log(`You're dead ${activePlayer.playerName}. Sorry, there is no afterlife :(`);
+                console.log("GAME OVER");
+                process.exit();
+            }
         }
-        battle = false;
     }
 }
 
-function attack(enemy, isFighting){
-if( enemy.enemyHP <= 0){
-console.log("Congrats you killed the " + wildEnemy + "!")
-// enemies can drop an item and you can push it into player inventory using 
+function walk() {
+    console.log(`*POP* 
+    We have removed your eyes. Now you must walk the maze in darkness! :P`);
+    
+    let keyInOption = readline.keyIn('Why not take a walk? ("w" = walk) You may also "q" = quit, or "p" = view profile    ');
+
+    if (keyInOption === 'p') {
+        console.log(activePlayer);
+    }
+
+    else if (keyInOption === 'q') {
+        console.log("I see... You're just another quitter. :[");
+        process.exit();
+    }
+
+    else if (keyInOption === 'w') {
+        console.log("You're walking");
+        let evilChaos = getRandomNum(3);
+        if (evilChaos === 2) {
+            return enemyEncounter();
+        }
+        else if (evilChaos !== 2) {
+        let lore = getRandomNum(scrollLore.length);
+            console.log(`There are many things you should know about the Black Scrolls...
+                        I've heard ${lore} 
+                        But why waste my breath on one such as you?
+                        Shall we continue walking?`);
+            return walk();
+        }
+    }
 }
+
+function victory() {
+    //if activePlayer get's all five scrolls (i.e. defeats all 5 enemies) the they win the game!
+
+}
+startGame();
+while (isAlive) {
+    walk();
 }
 
-// handles user's attack and enemy's HP value
-function userAttack() {
-    let damage = Math.floor(Math.random() * 15);
-    enemyHP - damage;
-    console.log(`You have ${userHP} hit points left.`);
-    console.log(`Your enemy has ${enemyHP} hit points left.`);
-}
-
-//handles enemy's attack and user's hp value
-function enemyAttack() {
-    let damage = Math.floor(Math.random() * 15);
-    userHP - damage;
-    console.log(`You have ${userHP} hit points left.`);
-    console.log(`Your enemy has ${enemyHP} hit points left.`);
-}
-
-
-
+//you can make a victory function that has text saying you win and also runs a process.exit()
 
