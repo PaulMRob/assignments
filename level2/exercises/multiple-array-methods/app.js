073c9984b6880facfa459450b0a2469da4882f39
PaulManRobertson <bigwallpaul514@gmail.com>
2022-10-04 13:28:17 -0600

    nesting multiple higher order functions into a function

---
+++ level2/exercises/multiple-array-methods/app.js
@@ -0,0 +1,67 @@
+const readline = require("readline-sync")
+
+var peopleArray = [
+    {
+        firstName: "Sarah",
+        lastName: "Palin",
+        age: 47
+    },
+    {
+        firstName: "Frank",
+        lastName: "Zappa",
+        age: 12
+    },
+    {
+        firstName: "Rick",
+        lastName: "Sanchez",
+        age: 78
+    },
+    {
+        firstName: "Morty",
+        lastName: "Smith",
+        age: 29
+    },
+    {
+        firstName: "Kyle",
+        lastName: "Mooney",
+        age: 27
+    },
+    {
+        firstName: "Pasha",
+        lastName: "Datsyuk",
+        age: 13
+    },
+    {
+        firstName: "Lev",
+        lastName: "Tolstoy",
+        age: 82
+    }
+]
+
+function sortedOfAge(arr) {
+    const filterAge = arr.filter(function (num) {
+        return num.age >= 18
+    });
+    const sorted = filterAge.sort(function (a, b) {
+        return a.lastName.localeCompare(b.lastName)
+    });
+
+    const embedded = sorted.map(function (num) {
+        return `<li> ${num.firstName} ${num.lastName} is ${num.age}</li>`
+    });
+
+    return embedded;
+}
+
+console.log(sortedOfAge(peopleArray));
+
+/*
+Output:
+[
+    "<li>Kyle Mooney is 27</li>",
+    "<li>Sarah Palin is 47</li>",
+    "<li>Rick Sanchez is 78</li>",
+    "<li>Morty Smith is 29</li>",
+    "<li>Lev Tolstoy is 82</li>"
+]
+*/
