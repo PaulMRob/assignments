9897f41a12c0b631c9cb78866aa73d72e3476487
PaulManRobertson <bigwallpaul514@gmail.com>
2022-10-17 14:56:09 -0600

    working through slope-style using es6 notation

--- level2/exercises/slope-style/app.js
+++ level2/exercises/slope-style/app.js
@@ -1,9 +1,78 @@
 const readline = require("readline-sync")
 
 // Green circle: Rest Operator
-let collectAnimals = (/*your code here*/) {
-    /*and here*/
+// 1)
+let collectAnimals = (...animals) => console.log(animals)
+
+collectAnimals("dog", "cat", "mouse", "jackolope", "platypus", "mouse", "beaver");
+
+// 2)
+let combineFruit = (fruit, sweets, vegetables) => { 
+    return { fruit, sweets, vegetables } 
 }
 
-collectAnimals("dog", "cat", "mouse", "jackolope", "platypus");
-// ["dog", "cat", "mouse", "jackolope", "platypus"]
+console.log(combineFruit(["apple", "pear"], ["cake", "pie"], ["carrot"]))
+
+// 3)
+function parseSentence({location, duration}) {
+    return `We're going to have a good time in ${location} for ${duration}`
+}
+
+console.log(parseSentence({
+    location: "Burly Idaho",
+    duration: "2 weeks"
+}))
+
+// 4)
+items = ["crysknife", "stilsuit", "thumper"]
+function returnFirst(items) { 
+    const [firstItem] = items
+    return firstItem
+}
+
+console.log(returnFirst(items))
+
+// 5)
+const favoriteActivities = ["napping", "playing in snow", "belly rubs", "rolling in grass", "eating"];
+
+function returnFavorites(arr) {
+    const [firstFav, secondFav, thirdfav] = arr
+    return `Meeka's top three favorite activities are, ${firstFav}, ${secondFav}, and ${thirdfav}`
+}
+console.log(returnFavorites(favoriteActivities))
+
+// BLUE SQUARE
+// 1)
+function combineAnimals(...args) {
+    let arr = []
+    args.forEach(args => {
+        arr = [...arr, ...args]
+    })
+    return arr
+}
+
+const realAnimals = ["dog", "cat", "mouse"];
+const magicalAnimals = ["jackolope"];
+const mysteriousAnimals = ["platypus"];
+const fakeAnimals = ["dragon", "orc", "troll"];
+
+console.log(combineAnimals(realAnimals, magicalAnimals, mysteriousAnimals, fakeAnimals))
+
+// BLACK DIAMOND
+// 1)
+let product = (a, b, c, d, e) => {
+    var numbers = [a, b, c, d, e];
+    return numbers.reduce((acc, number) =>  acc * number, 1)
+}
+
+console.log(product(1,2,3,4,5))
+
+// 2)
+let array = [6,7,8]
+let unshift = (a, b, c, d, e, array) => {
+    return [a, b, c, d, e, ...array]
+}
+
+console.log(unshift(...[1, 2, 3, 4, 5, array])) 
+
+// 3)
