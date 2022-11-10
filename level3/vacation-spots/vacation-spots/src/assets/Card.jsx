82006e31a87fb9fbfdfe9302932af1d2ca5f8ffe
PaulManRobertson <bigwallpaul514@gmail.com>
2022-11-10 14:45:23 -0700

    couldn't figure out how to conditionally render the colors so I added the color attribute to the data file.

--- level3/vacation-spots/vacation-spots/src/assets/Card.jsx
+++ level3/vacation-spots/vacation-spots/src/assets/Card.jsx
@@ -1,11 +1,23 @@
 import React from "react"
 
 export default function Card(props) {
+    let price = "$"
+    if (props.price > 500 && props.price < 1000) {
+        price = "$$"
+    } else if (props.price > 1000) {
+        price = "$$$"
+    }
+    
     return (
-        <div className="card">
-            <span>{props.place}</span>
-            <span>{props.price}</span>
-            <span>{props.timeToGo}</span>
+
+        <div className="card" style={{backgroundColor: props.color}}>
+            
+            <span className="place">{props.place}</span>
+            
+            <span className="price">{price}</span>
+            
+            <span className="time-to-go">{props.timeToGo}</span>
+            
         </div>
     )
 }
--- level3/vacation-spots/vacation-spots/src/data.jsx
+++ level3/vacation-spots/vacation-spots/src/data.jsx
@@ -1,26 +1,37 @@
 export default [
     {
+        id: 1,
         place: "Meridian, Idaho",
         price: 40,
-        timeToGo: "Spring"
+        timeToGo: "Spring",
+        color: "green"
     }, 
     {
+        id: 2,
         place: "Cancun",
         price: 900,
-        timeToGo: "Winter"
+        timeToGo: "Winter",
+        color: "blue"
     }, 
     {
-        place: "China", price: 1200,
-        timeToGo: "Fall"
+        id: 3,
+        place: "China", 
+        price: 1200,
+        timeToGo: "Fall",
+        color: "orange"
     }, 
     {
+        id: 4,
         place: "Russia",
         price: 1100,
-        timeToGo: "Summer"
+        timeToGo: "Summer",
+        color: "yellow"
     }, 
     {
+        id: 5,
         place: "Lebanon",
         price: 400,
-        timeToGo: "Spring"
+        timeToGo: "Spring",
+        color: "green"
     }
 ]
