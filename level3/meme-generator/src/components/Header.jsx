d5ea5438937111cdb18c2dfaa80a8b562e784f49
PaulManRobertson <bigwallpaul514@gmail.com>
2022-11-18 09:47:11 -0700

    made the header for the meme generator project

---
+++ level3/meme-generator/src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+export default function Header() {
+    return (
+        <header className="header">
+
+            <img src="./images/troll-face.png" />
+            <h1>Meme Generator</h1>
+
+            <p>React Course - Project 3</p>
+        </header>
+    )
+}
