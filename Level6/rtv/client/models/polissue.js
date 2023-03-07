ead9ef5ee4ae562b5871a8f63749b67014cdb543
PaulManRobertson <bigwallpaul514@gmail.com>
2023-03-07 12:50:57 -0700

    models folder setup

---
+++ Level6/rtv/client/models/polissue.js
@@ -0,0 +1,4 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+mondule.exports = mongoose.model("Polissue", polissueSchema);
--- Level6/rtv/server.js
+++ Level6/rtv/server.js
@@ -1,4 +1,3 @@
-const { application } = require("express");
 const express = require("express");
 const app = express();
 const morgan = require("morgan");
@@ -6,6 +5,11 @@ const mongoose = require("mongoose");
 
 app.use(express.json());
 app.use(morgan("dev"));
+
+mongoose.connect(process.env.MONGO_URL, () => {
+  console.log("Connected to the DB");
+});
+
 
 app.listen(9000, () => {
   console.log("Server is running on port 9000");
