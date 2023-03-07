3f00808dae92565af505f3cd342e090109dcd6c2
PaulManRobertson <bigwallpaul514@gmail.com>
2023-03-07 13:03:53 -0700

    finished server.js

--- Level6/rtv/server.js
+++ Level6/rtv/server.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
+require("dotenv").config();
 const morgan = require("morgan");
 const mongoose = require("mongoose");
+const { expressjwt } = require("express-jwt");
 
 app.use(express.json());
 app.use(morgan("dev"));
@@ -10,6 +12,17 @@ mongoose.connect(process.env.MONGO_URL, () => {
   console.log("Connected to the DB");
 });
 
+app.use("/auth", require("./routes/authRouter"));
+app.use("/api", expressjwt({ secret: process.env.SECRET, algorithms: ["HS256"] }));
+app.use("/api/polipost", require("./routes/polipostRouter"));
+
+app.use((err, req, res, next) => {
+    console.log(err)
+    if(err.name === "UnauthorizedError"){
+        res.status(err.status)
+    }
+    return res.send({errMsg: err.message})
+})
 
 app.listen(9000, () => {
   console.log("Server is running on port 9000");
