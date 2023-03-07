2797a423e184cc2df62f92cfbf0e850bb1eced5a
PaulManRobertson <bigwallpaul514@gmail.com>
2023-03-07 13:12:58 -0700

    models built out

--- Level6/rtv/models/polipost.js
+++ Level6/rtv/models/polipost.js
@@ -1,4 +1,22 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
+const polipostSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  Description: {
+    type: String,
+  },
+  imgUrl: {
+    type: String,
+  },
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+});
+
 mondule.exports = mongoose.model("Polipost", polipostSchema);
