7b92cf9456e019ed77e6a791b59659d70aade2c0
PaulManRobertson <bigwallpaul514@gmail.com>
2023-03-28 13:38:01 -0600

    bookpostRouter is built

---
+++ Level6/book-club/models/Bookpost.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const bookpostSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  quotation: {
+    type: String,
+    required: true,
+  },
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  upvotes: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  ],
+  downvotes: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  ],
+  timestamp: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("Bookpost", bookpostSchema);
